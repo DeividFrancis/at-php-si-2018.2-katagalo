0160b0acd13cb96033d5f9acb42ceb841a6a18e8
DioneRodrigues <dione.kacoal@gmail.com>
2018-09-19 20:57:43 -0300

    Revert "..."
    This reverts commit 34167d7db55e034fd509b07ce8f1bd959b254a46.

--- html/_partHTML/head.php
+++ html/_partHTML/head.php
@@ -7,7 +7,7 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <meta http-equiv="x-ua-compatible" content="ie=edge">
-<title><?=(isset($title) == false)? "Katagalo" : $title?></title>
+<title>Produtos Favoritos - KataGalo</title>
 
 <link rel="icon" type="image/png" href="images/icons/favicon.png" />
 
