2804c0c36c7cd8f22d7165e5341d46de3745228c
DeividF <deividfrancisdeoliveira@gmail.com>
2018-09-22 07:43:48 +0800

    arrumando cadastro

---
+++ public/cadProduto.php
@@ -0,0 +1,96 @@
+<?php 
+$title = "Cadastro pessoa";
+require_once "../config.php";
+require PROJECT_ROOT."/html/_partHTML/head.php" ?>
+
+<body class="hidden-sn elegant-color clearfix animated fadeIn">
+    <header>
+        <!-- Logout Modal-->
+        <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="logoutModalLabel"
+            aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title red-text" id="logoutModalLabel">Deseja Sair?</h5>
+                        <button class="close" type="button" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">X</span></button>
+                    </div>
+                    <div class="modal-body">Se você selecionar "SAIR", irá encerrar a sua sessão. Deseja mesmo assim?</div>
+                    <div class="modal-footer">
+                        <button class="btn btn-cyan btn-md col-md-4" type="button" data-dismiss="modal">Cancelar</button>
+                        <a class="btn btn-danger btn-md col-md-4" href="login.html">Sair</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <main class="container">
+        <div class="card">
+            <h6 class="card-header info-color-dark py-3">
+                <p class="my-2 h5-responsive white-text text-center font-weight-bold">Escolha opção para cadastro</p>
+            </h6>
+
+            <!--Card content-->
+            <div class="card-body">
+
+                <?php
+					include PROJECT_ROOT.'/html/classes/Form.class.php';
+					include PROJECT_ROOT.'/html/classes/Field.class.php';
+					include PROJECT_ROOT.'/html/classes/Select.class.php';
+					
+					$form = new Form("Produto");
+					$f1 = new Field("Barcode", "barcode");
+					$f2 = new Field("Descrição", "descricao");
+					$f3 = new Field("Preço (R$)", "preco");
+
+					$f4 = new Select("Categoria", 'procId');
+					$f4->addOption("Categoria 1", 1);
+					$f4->addOption("Categoria 2", 2);
+					$f4->addOption("Categoria 3", 3);
+
+					$form->addRow(1,array($f1, $f4));
+					$form->addRow(2,array($f2, $f3));
+					$form->addButtom("Salvar", "salvar");
+					
+					echo $form->getHtml();
+					?>
+
+            </div>
+    </main>
+    <!-- JQuery -->
+    <script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.0/jquery.mask.js"></script>
+
+    <!-- Tooltips -->
+    <script type="text/javascript" src="js/popper.min.js"></script>
+
+    <!-- Bootstrap core JavaScript -->
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+
+    <!-- MDB core JavaScript -->
+    <script type="text/javascript" src="js/mdb.min.js"></script>
+
+    <!-- Outros JS's -->
+    <script type="text/javascript" src="js/limiteDeCaracteres.js"></script>
+    <script type="text/javascript" src="js/ajax.js"></script>
+    <script type="text/javascript" src="js/mascaras.js"></script>
+
+    <script>
+        // Material Select Initialization
+        $(document).ready(function () {
+            $('.mdb-select').material_select();
+        });
+
+        // SideNav Initialization
+        $(".button-collapse").sideNav();
+
+        new WOW().init();
+
+        // Tooltips Initialization
+        $(function () {
+            $('[data-toggle="tooltip"]').tooltip()
+        })
+    </script>
+</body>
+
+</html>
--- public/cadastros/cadastro-produto.php
+++ public/cadastros/cadastro-produto.php
@@ -1,4 +1,27 @@
-<?php require_once "../header.php"; ?>
+<?php 
+require_once "../../config.php";
+require_once PROJECT_ROOT."/html/_partHTML/head.php"; ?>
+
+<body class="hidden-sn elegant-color clearfix animated fadeIn">
+	<header>
+		<!-- Logout Modal-->
+		<div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="logoutModalLabel" aria-hidden="true">
+			<div class="modal-dialog" role="document">
+				<div class="modal-content">
+					<div class="modal-header">
+						<h5 class="modal-title red-text" id="logoutModalLabel">Deseja Sair?</h5>
+						<button class="close" type="button" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">X</span></button>
+					</div>
+					<div class="modal-body">Se você selecionar "SAIR", irá encerrar a sua sessão. Deseja mesmo assim?</div>
+					<div class="modal-footer">
+						<button class="btn btn-cyan btn-md col-md-4" type="button" data-dismiss="modal">Cancelar</button>
+						<a class="btn btn-danger btn-md col-md-4" href="login.html">Sair</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</header>
+
 	<main>
 		<div class="container">
 			<div class="card card-register mx-auto mt-5 animated bounceIn">
@@ -26,9 +49,46 @@
 					$form->addButtom("Salvar", "salvar");
 					
 					echo $form->getHtml();
-					?>						
+					?>
 				</div>
 			</div>
 		</div>
 	</main>
-<?php require_once '../footer.php';
+	<!-- JQuery-->
+	<script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
+	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.0/jquery.mask.js"></script>
+
+	<!-- Tooltips -->
+	<script type="text/javascript" src="js/popper.min.js"></script>
+
+	<!-- Bootstrap core JavaScript -->
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+
+	<!-- MDB core JavaScript -->
+	<script type="text/javascript" src="js/mdb.min.js"></script>
+
+	<!-- Outros JS's -->
+	<script type="text/javascript" src="js/limiteDeCaracteres.js"></script>
+	<script type="text/javascript" src="js/ajax.js"></script>
+	<script type="text/javascript" src="js/mascaras.js"></script>
+
+	<script>
+		// Material Select Initialization
+		$(document).ready(function () {
+			$('.mdb-select').material_select();
+		});
+
+		// SideNav Initialization
+		$(".button-collapse").sideNav();
+
+		new WOW().init();
+
+		// Tooltips Initialization
+		$(function () {
+			$('[data-toggle="tooltip"]').tooltip()
+		})
+	</script>
+</body>
+
+</html>
+<?php //require_once PROJECT_ROOT."/html/_partHTML/footer.php"; ?>
